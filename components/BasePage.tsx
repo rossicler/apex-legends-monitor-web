8f37131874e2bb1d3bf54cb46ddd45969f74423a
Thiago <thiago099carvalho@gmail.com>
2022-10-12 20:48:32 -0300

    create global style with styled components

---
+++ components/BasePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// components
+import { Container, NavList, NavItem } from "./Navbar.styles";
+
+const BasePage = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default BasePage;
---
+++ styles/themes/theme.js
@@ -0,0 +1,11 @@
+export default {
+  spacing : {
+    small: 8,
+    medium: 16,
+    large: 24
+  },
+  borderRadius: '10px',
+  primaryColor: '#E5E1D8',
+  secondaryColor: '#000',
+  textColor: '#fff',
+};
