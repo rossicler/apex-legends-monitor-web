f2e4d5222467be926c66fe7760ac4d698c1a1aa9
Thiago <thiago099carvalho@gmail.com>
2022-10-13 19:28:34 -0300

    create store page

---
+++ pages/store.tsx
@@ -0,0 +1,12 @@
+import CharBanner from "../components/CharBanner";
+import Navbar from "../components/Navbar";
+
+function Store() {
+  return( 
+    <>
+      <Navbar/>
+      <CharBanner/>
+    </>
+)}
+
+export default Store
